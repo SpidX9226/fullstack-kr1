46a733da433656b624541a37af54a6519a1bff6a
Said <saidmahov10@gmail.com>
2025-09-26 21:42:55 +0300

    i nate higgers

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Happy developing ✨')
+console.log('Happy developing ✨');
+
+let variable = document.getElementById('variable');
+
+if (variable == 'variable') {
+    console.log(variable);
+} else {
+    console.log('svo');
+}
